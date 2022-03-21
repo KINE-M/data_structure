dae1eca3dfe5b3781cf3843cc5e265365dcc507d
KINE-M <a.m.cassandre.k@gmail.com>
2022-03-21 22:07:40 +0900

    feat(linked_list): add reverse method

--- linked_list.py
+++ linked_list.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any
+from typing import Any, Optional
 
 
 class Node(object):
@@ -52,18 +52,63 @@ class LinkedList(object):
         previous_node.next = current_node.next
         current_node = None
 
+    def reverse_iterative(self) -> None:
+        previous_node = None
+        current_node = self.head
+        while current_node:
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+        self.head = previous_node
+
+    def reverse_recursive(self) -> None:
+        def _reverse_recursive(current_node: Node, previous_node) -> Node:
+            if not current_node:
+                return previous_node
+
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+            return _reverse_recursive(current_node, previous_node)
+
+        self.head = _reverse_recursive(self.head, None)
+
+    def reverse_even(self) -> None:
+        def _reverse_even(head: Node, previous_node: Node) -> Optional[Node]:
+            if head is None:
+                return None
+
+            current_node = head
+            while current_node and current_node.data % 2 == 0:
+                next_node = current_node.next
+                current_node.next = previous_node
+                previous_node = current_node
+                current_node = next_node
+
+            if current_node != head:
+                head.next = current_node
+                _reverse_even(current_node, None)
+                return previous_node
+            else:
+                head.next = _reverse_even(head.next, head)
+                return head
+
+        self.head = _reverse_even(self.head, None)
+
 
 if __name__ == '__main__':
     l = LinkedList()
     l.append(1)
-    l.append(2)
     l.append(3)
-    l.insert(0)
-    # print(l.head.data)
-    # print(l.head.next.data)
-    # print(l.head.next.next.data)
-    # print(l.head.next.next.next.data)
-    l.print()
-    print('##################')
-    l.remove(2)
+    l.append(2)
+    l.append(4)
+    l.append(6)
+    l.append(8)
+    l.append(3)
+    l.append(4)
+    l.append(8)
+    l.append(10)
+    l.reverse_even()
     l.print()
